f73f619fac3c1f7d65ac8981786fcc78fbac3e9c
Sorawong L <Sorawong2543@gmail.com>
2023-09-10 11:59:51 +0700

    feat: hide bg in md screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         height={1497}
         src={'/images/background/city.png'}
         alt="bg"
-        className="absolute -bottom-[calc(100dvh-90%)] left-0"
+        className="absolute -bottom-[calc(100dvh-90%)] left-0 md:hidden"
       />
     </div>
   )
