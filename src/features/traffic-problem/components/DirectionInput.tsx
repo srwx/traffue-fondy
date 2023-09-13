0e71505c78a349db0da710eb09cb61edac0030d9
Sorawong L <Sorawong2543@gmail.com>
2023-09-13 22:25:26 +0700

    feat: auto search

--- src/features/traffic-problem/components/DirectionInput.tsx
+++ src/features/traffic-problem/components/DirectionInput.tsx
@@ -3,7 +3,7 @@ import { PinIcon, SearchIcon } from '@/icons'
 import { simplifyRoute } from '@/utils/simplifyRoute'
 import { Autocomplete } from '@react-google-maps/api'
 import clsx from 'clsx'
-import React, { useCallback, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 interface DirectionInputProps {
   setDirectionsResponse: React.Dispatch<
@@ -29,43 +29,67 @@ const DirectionInput = (props: DirectionInputProps) => {
     setRouteCoordinates,
   } = props
 
-  const startPointRef = useRef<HTMLInputElement>(null)
-  const endPointRef = useRef<HTMLInputElement>(null)
+  const [originPoint, setOriginPoint] =
+    useState<google.maps.places.PlaceResult | null>(null)
+  const [destinationPoint, setDestinationPoint] =
+    useState<google.maps.places.PlaceResult | null>(null)
 
-  const calculateRoute = useCallback(async () => {
-    if (!startPointRef.current?.value || !endPointRef.current?.value) return
+  const originAutocomplete = useRef<google.maps.places.Autocomplete | null>(
+    null
+  )
+  const destinationAutocomplete =
+    useRef<google.maps.places.Autocomplete | null>(null)
 
-    const directionsService = new google.maps.DirectionsService()
-    const result = await directionsService.route({
-      origin: startPointRef.current.value,
-      destination: endPointRef.current.value,
-      travelMode: google.maps.TravelMode.DRIVING,
-    })
+  useEffect(() => {
+    const calculateRoute = async () => {
+      const directionsService = new google.maps.DirectionsService()
+      const result = await directionsService.route({
+        origin: {
+          lat: originPoint?.geometry?.location?.lat() || 0,
+          lng: originPoint?.geometry?.location?.lng() || 0,
+        },
+        destination: {
+          lat: destinationPoint?.geometry?.location?.lat() || 0,
+          lng: destinationPoint?.geometry?.location?.lng() || 0,
+        },
+        travelMode: google.maps.TravelMode.DRIVING,
+      })
 
-    const tempRoute: { lat: number; lng: number }[] = []
-    result.routes[0].legs.forEach((leg) => {
-      leg.steps.forEach((step) => {
-        step.path.forEach((point) => {
-          tempRoute.push({
-            lat: point.lat(),
-            lng: point.lng(),
+      const tempRoute: { lat: number; lng: number }[] = []
+      result.routes[0].legs.forEach((leg) => {
+        leg.steps.forEach((step) => {
+          step.path.forEach((point) => {
+            tempRoute.push({
+              lat: point.lat(),
+              lng: point.lng(),
+            })
           })
         })
       })
-    })
 
-    const simplified = simplifyRoute(tempRoute, 0.001)
-    setRouteCoordinates(simplified)
+      const simplified = simplifyRoute(tempRoute, 0.001)
+      setRouteCoordinates(simplified)
 
-    setDirectionsResponse(result)
-    setDistance(result.routes[0].legs[0].distance?.text || '-')
-    setDuration(result.routes[0].legs[0].duration?.text || '-')
-  }, [setDirectionsResponse, setDistance, setDuration, setRouteCoordinates])
+      setDirectionsResponse(result)
+      setDistance(result.routes[0].legs[0].distance?.text || '-')
+      setDuration(result.routes[0].legs[0].duration?.text || '-')
+    }
+
+    if (!originPoint || !destinationPoint) return
+
+    calculateRoute()
+  }, [
+    destinationPoint,
+    originPoint,
+    setDirectionsResponse,
+    setDistance,
+    setDuration,
+    setRouteCoordinates,
+  ])
 
   return (
     <div
       className={clsx(
-        //   'absolute bottom-0 left-0',
         'relative',
         'w-full bg-[#F6F6F6] z-10',
         'flex flex-col gap-y-[10px] py-3 px-4'
@@ -79,9 +103,18 @@ const DirectionInput = (props: DirectionInputProps) => {
         <PinIcon />
         <div className="flex flex-col gap-y-1 w-full">
           <span className="text-sm text-[#7A7A7A]">ตำแหน่งที่ตั้งของคุณ</span>
-          <Autocomplete>
+          <Autocomplete
+            onLoad={(autocomplete) => {
+              originAutocomplete.current = autocomplete
+            }}
+            onPlaceChanged={() => {
+              if (originAutocomplete.current) {
+                const place = originAutocomplete.current.getPlace()
+                setOriginPoint(place)
+              }
+            }}
+          >
             <Input
-              ref={startPointRef}
               placeholder="พิมพ์ตำแหน่งที่ตั้งของคุณ"
               leftIcon={<SearchIcon />}
             />
@@ -92,21 +125,21 @@ const DirectionInput = (props: DirectionInputProps) => {
         <PinIcon stroke="#F94A00" />
         <div className="flex flex-col gap-y-1 w-full">
           <span className="text-sm text-[#7A7A7A]">ไปยัง</span>
-          <Autocomplete>
-            <Input
-              ref={endPointRef}
-              placeholder="พิมพ์จุดหมายของคุณ"
-              leftIcon={<SearchIcon />}
-            />
+          <Autocomplete
+            onLoad={(autocomplete) =>
+              (destinationAutocomplete.current = autocomplete)
+            }
+            onPlaceChanged={() => {
+              if (destinationAutocomplete.current) {
+                const place = destinationAutocomplete.current.getPlace()
+                setDestinationPoint(place)
+              }
+            }}
+          >
+            <Input placeholder="พิมพ์จุดหมายของคุณ" leftIcon={<SearchIcon />} />
           </Autocomplete>
         </div>
       </div>
-      <button
-        className="w-full py-2 rounded-lg text-center bg-primary text-white"
-        onClick={calculateRoute}
-      >
-        คำนวนระยะทาง
-      </button>
     </div>
   )
 }
